c9f5a90a49e098eeb091ffa30c0acd10d5162998
柳树怪 <wolliw@163.com>
2022-09-30 15:51:32 +0800

    移植至1.19，可运行

--- src/main/java/willow/train/kuayue/Items/foods/Food1.java
+++ src/main/java/willow/train/kuayue/Items/foods/Food1.java
@@ -1,7 +1,6 @@
 package willow.train.kuayue.Items.foods;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -20,7 +19,7 @@ public class Food1 extends Item {
 
     @Override
     public void appendHoverText(@NotNull ItemStack p_41421_, @Nullable Level p_41422_, List<Component> tooltips, TooltipFlag p_41424_) {
-        tooltips.add(new TranslatableComponent("item.kuayue.train_diet_1.tooltip"));
+        tooltips.add(Component.translatable("item.kuayue.train_diet_1.tooltip"));
         super.appendHoverText(p_41421_, p_41422_, tooltips, p_41424_);
     }
 }
